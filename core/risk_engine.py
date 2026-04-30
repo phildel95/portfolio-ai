9813c9d7f480e8ded1e59732973f79ed278cc0f2
phildel95 <philippe.delouis@sfr.fr>
2026-04-30 23:21:35 +0200

    Update risk_engine.py

--- core/risk_engine.py
+++ core/risk_engine.py
@@ -1,3 +1,14 @@
+def risk_score(perf, vol):
+    score = 0
 
+    if perf > 20:
+        score -= 1
+    if perf < -10:
+        score += 2
+
+    if vol > 3:
+        score += 1
+
+    return score
 
 
